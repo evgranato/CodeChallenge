eaf43c077859a0423f436d897dd36dd3eafbf132
Evan Granato <evgranato@gmail.com>
2021-08-08 07:09:01 -0500

    refactored

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const button1 = document.querySelector(".button1")
 const button2 = document.querySelector(".button2")
+const button1 = document.querySelector(".button1")
 const buttonGroup = document.querySelector(".buttons")
 const nameInput = document.querySelector("#personName")
 const nameLabel = document.querySelector(".personName")
@@ -14,44 +14,17 @@ const data =
         location: null
     }
 
-
-
-button1.addEventListener('click', () => {
-    button1.classList.add("highlight")
-})
-
 nameInput.addEventListener('keydown', (e) => {
     if(e.key !== 'Enter'){
-        console.log(e)
     } else {
         data.name = nameInput.value;
-        nameLabel.remove()
-        nameInput.remove()
-        
-        const travelLabel = document.createElement("label");
-        travelLabel.for = 'personTravel';
-        travelLabel.innerText = 'Where would you like to travel?';
-        travelLabelSelect.append(travelLabel)
-        // const travelInput = document.createElement('input')
-        // travelInput.setAttribute("aria-describedby", "personTravel");
-        // travelInput.classList.add("form-control");
-        // travelInput.id = "personTravel";
-        // travelInput.type = "text";
-        // locationGroup.append(travelInput)
-        locationGroup.style.visibility = "visible"
+        nameGroup.remove()
+        locationGroup.style.display = ""
         button1.classList.add("highlight")
-        const travelLabel2 = document.createElement("small");
-        const classes2 = ["form-text", "text-muted"]
-        travelLabel2.id = 'personTravel';
-        travelLabel2.innerText = 'If you could travel ANYWHERE you wanted...';
-        travelLabel2.classList.add(...classes2)
-        locationGroup.append(travelLabel2)
     }
 })
-
 locationInput.addEventListener('keydown', (e) => {
     if(e.key !== 'Enter'){
-        console.log(e)
     } else {
         data.location = locationInput.value
         button2.classList.add("highlight")
@@ -60,10 +33,11 @@ locationInput.addEventListener('keydown', (e) => {
         subButton.classList.add(...classes)
         subButton.innerText = "Submit"
         buttonGroup.append(subButton)
-        console.log(data)
         const theGoogleButton = document.querySelector('.subButton')
         theGoogleButton.addEventListener('click', () => {
-            window.open(`https://google.com/search?q=${data.location}`)
+            window.open(`https://google.com/search?q=${data.location}`);
+            theGoogleButton.classList.add("highlight")
         })
     }
 })
+
